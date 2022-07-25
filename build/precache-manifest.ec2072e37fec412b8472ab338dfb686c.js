03fc2b345de48da64b3de53c86e20aa8d0c14d36
lensatesema <lensatesema19@gmail.com>
2022-07-25 23:32:29 +0300

    initial commit

---
+++ build/precache-manifest.ec2072e37fec412b8472ab338dfb686c.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "155284e9bc923581cc6a43a6f709103a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac8d697740edb9af0ea6",
+    "url": "/static/css/main.f0f37730.chunk.css"
+  },
+  {
+    "revision": "e6a6c56b7d5d233ea663",
+    "url": "/static/js/2.9801b060.chunk.js"
+  },
+  {
+    "revision": "8a688642fc4627fb503fa44b3ff6ceb8",
+    "url": "/static/js/2.9801b060.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac8d697740edb9af0ea6",
+    "url": "/static/js/main.e747e042.chunk.js"
+  },
+  {
+    "revision": "0bb32636109e657aeb29",
+    "url": "/static/js/runtime-main.f17a7f60.js"
+  },
+  {
+    "revision": "1cd845d6f5a1445d85db5f4f31baf865",
+    "url": "/static/media/Arcon-lg.1cd845d6.jpg"
+  },
+  {
+    "revision": "6876fc6527efcaa8ae1386693dce73d5",
+    "url": "/static/media/Card_large.6876fc65.jpg"
+  },
+  {
+    "revision": "45b2503e8bb62934357a42699c1f33fc",
+    "url": "/static/media/Card_small.45b2503e.jpg"
+  },
+  {
+    "revision": "5465cc591da2ac562ae16bb33f3575b8",
+    "url": "/static/media/apple.5465cc59.svg"
+  },
+  {
+    "revision": "aea2d7189d99437b77d399f64935c8f3",
+    "url": "/static/media/bag.aea2d718.svg"
+  },
+  {
+    "revision": "d34f65aa590c8d3a0928fb07f2d1289b",
+    "url": "/static/media/central_park_lg.d34f65aa.jpg"
+  },
+  {
+    "revision": "edf94f2ff598948e0d9fdb0611b2cf0c",
+    "url": "/static/media/central_park_sm.edf94f2f.jpg"
+  },
+  {
+    "revision": "a7ef5f4882048c96dcefc8239b534c8e",
+    "url": "/static/media/iphone11-colored.a7ef5f48.jpg"
+  },
+  {
+    "revision": "573a6b368484e06a1543fd0af4d70bfa",
+    "url": "/static/media/iphone11-pro-bg.573a6b36.jpg"
+  },
+  {
+    "revision": "30b06e91d5c36c592794c16ca7e42ae7",
+    "url": "/static/media/ipodPronew-sm.30b06e91.jpg"
+  },
+  {
+    "revision": "f7f2ab1f61fd401300c25a2df3be6893",
+    "url": "/static/media/ipodPronew.f7f2ab1f.jpg"
+  },
+  {
+    "revision": "851443b3e3a4a5ea65c4e33622cc4c42",
+    "url": "/static/media/macbookair-new-sm.851443b3.jpg"
+  },
+  {
+    "revision": "890634a4409ae22865fbad51d6004953",
+    "url": "/static/media/macbookair-new.890634a4.jpg"
+  },
+  {
+    "revision": "8dd1210f9d7be403307a214472c87f03",
+    "url": "/static/media/search.8dd1210f.svg"
+  },
+  {
+    "revision": "3dda7621799aa621c3d6df229700440e",
+    "url": "/static/media/spyder.3dda7621.jpg"
+  },
+  {
+    "revision": "aa3dc7331ab40ebb0b984342d3edb186",
+    "url": "/static/media/watch.aa3dc733.jpg"
+  }
+]);
